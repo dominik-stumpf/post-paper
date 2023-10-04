94916520ffa5f01f67a3090ccf5f8cf23ac46e83
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-04 20:16:55 +0200

    added login and middleware for csp

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,9 @@
 'use client';
+import { api, seedUserData } from '@/server-data';
+import { signIn } from 'next-auth/react';
+import PocketBase from 'pocketbase';
+
+import { FormEvent } from 'react';
 
 // import { signIn, useSession } from 'next-auth/react';
 // import { useEffect } from 'react';
@@ -14,43 +19,75 @@ export default function Page() {
   //     console.log('signin response', data);
   //   })();
   // }, []);
+  async function signUp(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const { password, email } = Object.fromEntries(formData.entries());
+    const pb = new PocketBase(api);
+    // const data = {
+    //   // username: 'test_username',
+    //   email: email,
+    //   // emailVisibility: false,
+    //   password: password,
+    //   passwordConfirm: password,
+    //   // name: 'test',
+    // };
+    const resp = await pb
+      .collection('users')
+      .authWithPassword(seedUserData.email, seedUserData.password);
+    console.log(resp);
+  }
+  async function signInGithub() {
+    await signIn('github');
+  }
 
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse lg:gap-16">
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form className="card-body" onSubmit={signUp}>
               <div className="form-control">
-                <label className="label">
+                <label className="label" htmlFor="email">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
                   placeholder="email"
+                  name="email"
+                  id="email"
                   className="input input-bordered"
                   required
                 />
               </div>
               <div className="form-control">
-                <label className="label">
+                <label className="label" htmlFor="password">
                   <span className="label-text">Password</span>
                 </label>
                 <input
                   type="password"
                   placeholder="password"
+                  name="password"
+                  id="password"
                   className="input input-bordered"
                   required
                 />
-                <label className="label">
-                  <a href="#1" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
               </div>
+              <label className="label">
+                <a href="#1" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
               <div className="form-control mt-6">
                 <button className="btn btn-primary" type="submit">
-                  Login
+                  Sign up
+                </button>
+                <button
+                  className="btn btn-primary mt-4"
+                  type="button"
+                  onClick={signInGithub}
+                >
+                  github
                 </button>
               </div>
             </form>
