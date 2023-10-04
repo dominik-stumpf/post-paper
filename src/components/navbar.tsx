087a2cca864308a4eefbe3685a7a3b7a4f1f65e0
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-04 16:37:36 +0200

    extracted navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,85 @@
+import Link from 'next/link';
+
+export function Navbar() {
+  return (
+    <div className="navbar bg-base-100 sticky top-0">
+      <div className="navbar-start">
+        {/* <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <title>chevron</title>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div> */}
+        <Link className="btn btn-ghost normal-case text-xl" href="/">
+          post-paper
+        </Link>
+      </div>
+      {/* <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li tabIndex={0}>
+                <details>
+                  <summary>Parent</summary>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div> */}
+      <div className="navbar-end flex gap-4">
+        <Link className="btn" href="/login">
+          login
+        </Link>
+        <Link className="btn btn-primary" href="/signup">
+          sign up
+        </Link>
+      </div>
+    </div>
+  );
+}
