b427689381afe1499c9fcc3feff056ea12da24d3
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-10 19:20:22 +0200

    added type, removed unused oauths

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,12 @@
 import { config } from '@/app/api/auth/lib/auth';
 import { SupabaseAdapter } from '@auth/supabase-adapter';
 import NextAuth from 'next-auth/next';
+import jwt from 'jsonwebtoken';
 
 const envKeys = [
   'NEXT_PUBLIC_SUPABASE_URL',
   'SUPABASE_SERVICE_ROLE_KEY',
+  'SUPABASE_JWT_SECRET',
 ] as const;
 
 for (const envKey of envKeys) {
@@ -28,37 +30,21 @@ const handler = NextAuth({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
   }),
+  callbacks: {
+    async session({ session, user }) {
+      const signingSecret = process.env.SUPABASE_JWT_SECRET;
+      if (signingSecret) {
+        const payload = {
+          aud: 'authenticated',
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: 'authenticated',
+        };
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+      }
+      return session;
+    },
+  },
 });
 export { handler as GET, handler as POST };
-
-// import NextAuth from "next-auth"
-// import { SupabaseAdapter } from "@auth/supabase-adapter"
-// import jwt from "jsonwebtoken"
-
-// // For more information on each option (and a full list of options) go to
-// // https://authjs.dev/reference/configuration/auth-options
-// export default NextAuth({
-//   // https://authjs.dev/reference/providers/oauth-builtin
-//   providers: [...],
-//   adapter: SupabaseAdapter({
-//     url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-//     secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
-//   }),
-//     callbacks: {
-//     async session({ session, user }) {
-//       const signingSecret = process.env.SUPABASE_JWT_SECRET
-//       if (signingSecret) {
-//         const payload = {
-//           aud: "authenticated",
-//           exp: Math.floor(new Date(session.expires).getTime() / 1000),
-//           sub: user.id,
-//           email: user.email,
-//           role: "authenticated",
-//         }
-//         session.supabaseAccessToken = jwt.sign(payload, signingSecret)
-//       }
-//       return session
-//     },
-//   },
-//   // ...
-// })
