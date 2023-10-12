1e03cee12819c6e61a92f6e4b7ded205f4a7c857
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-12 12:52:05 +0200

    added react-lenis to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import '@/types/validate-env-vars';
+import { LenisWrapper } from '@/components/lenis-wrapper';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen bg-background flex flex-col items-center text-foreground">
-          {children}
-        </main>
+        <LenisWrapper>
+          <main className="min-h-screen bg-background flex flex-col items-center text-foreground">
+            {children}
+          </main>
+        </LenisWrapper>
       </body>
     </html>
   );
