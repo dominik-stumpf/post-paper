94bd9a69c8095e6a053ad4f3326226bed1e18041
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-15 23:03:02 +0200

    main page update to use rpc

--- components/paper-card.tsx
+++ components/paper-card.tsx
@@ -2,45 +2,19 @@ import Link from 'next/link';
 import { Avatar } from './avatar';
 
 interface PaperCardProps {
-  content: string;
-  created_at: string;
-  id: string;
-  title: string;
-  // user_id: string;
-  profiles: {
-    avatar_url: string;
-    // id: string;
-    name: string;
-    // user_name: string;
-  } | null;
-  likes: {
-    // created_at: string;
-    // id: number;
-    count: number;
-    // post_id: string;
-    // user_id: string;
-  }[];
+  data: Database['public']['Functions']['get_post_list']['Returns'][0];
 }
 
 export function PaperCard({
-  content,
-  title,
-  created_at,
-  profiles,
-  id,
-  likes,
+  data: { content, title, created_at, name, avatar_url, id, likes_count },
 }: PaperCardProps) {
-  if (profiles === null) {
-    return null;
-  }
-
   return (
     <Link href={`/paper/${id}`} className="w-1/2 max-w-2xl">
       <section className="flex flex-col w-full gap-4 p-4 border">
         <div className="flex">
           <div className="flex items-center gap-2 grow">
-            <Avatar imageSrc={profiles.avatar_url} />
-            <div>{profiles.name}</div>
+            <Avatar imageSrc={avatar_url} />
+            <div>{name}</div>
             <time dateTime={created_at}>
               {new Date(created_at).toLocaleString('en-US', {
                 month: 'short',
@@ -48,7 +22,7 @@ export function PaperCard({
               })}
             </time>
           </div>
-          <div className="grow-0">{likes[0].count}</div>
+          <div className="grow-0">{likes_count}</div>
         </div>
         <div className="flex flex-col gap-3">
           <h2 className="text-2xl font-bold overflow-clip max-h-16">{title}</h2>
