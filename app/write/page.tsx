367937c033d56dc4caed838d31916f3326552922
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-17 18:12:19 +0200

    added lexical as editor to /write

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,16 +1,11 @@
-import {
-  createServerActionClient,
-  createServerComponentClient,
-} from '@supabase/auth-helpers-nextjs';
-import { revalidatePath } from 'next/cache';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { ContentInput } from './mdr/content-input';
+'use client';
 
-export const dynamic = 'force-dynamic';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { redirect } from 'next/navigation';
+import { Editor } from './editor';
 
 export default async function Page() {
-  const supabase = createServerComponentClient<Database>({ cookies });
+  const supabase = createClientComponentClient<Database>();
   const {
     data: { session },
   } = await supabase.auth.getSession();
@@ -19,50 +14,9 @@ export default async function Page() {
     redirect('/');
   }
 
-  async function submitPost(formData: FormData) {
-    'use server';
-
-    const supabase = createServerActionClient<Database>({ cookies });
-    const {
-      data: { session },
-      error: authError,
-    } = await supabase.auth.getSession();
-    const { title, content } = Object.fromEntries(formData.entries());
-
-    if (authError || session?.user === undefined) {
-      redirect('/');
-    }
-
-    const { user } = session;
-
-    if (typeof content === 'string' && typeof title === 'string') {
-      await supabase.from('posts').insert({ content, title, user_id: user.id });
-      revalidatePath('/');
-    }
-  }
-
   return (
-    <>
-      <form
-        action={submitPost}
-        className="flex gap-4 py-16 flex-col w-1/3 max-w-3xl"
-      >
-        {/* <label htmlFor="title">title</label>
-        <InputField name="title" id="title" /> */}
-        {/* <label id="content">content</label> */}
-        <ContentInput />
-        <button type="submit">post</button>
-      </form>
-    </>
+    <form>
+      <Editor />
+    </form>
   );
 }
-
-// function InputField({ name, id }: { name: string; id: string }) {
-//   return (
-//     <input
-//       name={name}
-//       id={id}
-//       className="p-2 bg-black outline-none ring-1 ring-white w-full"
-//     />
-//   );
-// }
---
+++ app/write/submit-post-action.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export async function submitPost(formData: FormData) {
+  const supabase = createServerActionClient<Database>({ cookies });
+  const {
+    data: { session },
+    error: authError,
+  } = await supabase.auth.getSession();
+  const { title, content } = Object.fromEntries(formData.entries());
+
+  if (authError || session?.user === undefined) {
+    redirect('/');
+  }
+
+  const { user } = session;
+
+  if (typeof content === 'string' && typeof title === 'string') {
+    await supabase.from('posts').insert({ content, title, user_id: user.id });
+    revalidatePath('/');
+  }
+}
