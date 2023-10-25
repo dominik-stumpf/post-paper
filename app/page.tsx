14419dcff00b44b89df1e362cf0dfeb06235e879
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-25 17:54:03 +0200

    select is replaced with rpc

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,19 @@ export default async function Index() {
   );
 
   const { data: posts } = await supabase
-    // .rpc('get_post_list')
-    .from('posts')
-    .select('paper_data, created_at, id, likes(id), profiles(name, avatar_url)')
+    .rpc('get_post_list')
+    // .from('posts')
+    // .select('paper_data, created_at, id, likes(id), profiles(name, avatar_url)')
     .order('created_at', { ascending: false })
     .range(0, 10);
 
-  // console.log(posts[0].like_count);
+  // console.log(posts);
 
   return (
     <div className="flex flex-col items-center w-full">
       <div className="grid w-full gap-12 py-12 place-items-center">
         {posts?.map((post) => (
-          <PaperCard key={post.id} {...post} />
+          <PaperCard key={post.id} data={post} />
         ))}
       </div>
     </div>
--- components/paper-card.tsx
+++ components/paper-card.tsx
@@ -2,50 +2,24 @@ import Link from 'next/link';
 import { Avatar } from './avatar';
 import { Prose } from './prose';
 import Markdown from 'react-markdown';
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import { fromMarkdown } from 'mdast-util-from-markdown';
-import { Fragment, ReactNode } from 'react';
 import { PaperParser } from '@/utils/paper-parser';
 
-// interface PaperCardProps {
-//   data: Database['public']['Functions']['get_post_list']['Returns'][0];
-// }
-
 interface PaperCardProps {
-  paper_data: string;
-  created_at: string;
-  id: string;
-  likes: {
-    id: number;
-  }[];
-  profiles: {
-    name: string;
-    avatar_url: string;
-  } | null;
+  data: Database['public']['Functions']['get_post_list']['Returns'][0];
 }
-[];
 
 export async function PaperCard({
-  created_at,
-  profiles,
-  id,
-  likes,
-  paper_data,
+  data: { created_at, avatar_url, name, id, likes_count, truncated_paper_data },
 }: PaperCardProps) {
-  if (profiles === null) return;
-
-  const { title, content } = new PaperParser(
-    paper_data.slice(0, 256),
-  ).parseCard();
+  const { title, content } = new PaperParser(truncated_paper_data).parseCard();
 
   return (
-    <Link href={`/paper/${id}`} scroll={false} className="w-1/2 max-w-2xl">
+    <Link href={`/paper/${id}`} className="w-1/2 max-w-2xl">
       <section className="flex flex-col w-full gap-4 p-4 border">
         <div className="flex">
           <div className="flex items-center gap-2 grow">
-            <Avatar imageSrc={profiles?.avatar_url} />
-            <div>{profiles?.name}</div>
+            <Avatar imageSrc={avatar_url} />
+            <div>{name}</div>
             <time dateTime={created_at}>
               {new Date(created_at).toLocaleString('en-US', {
                 month: 'short',
@@ -53,7 +27,7 @@ export async function PaperCard({
               })}
             </time>
           </div>
-          <div className="grow-0">{likes.length}</div>
+          <div className="grow-0">{likes_count}</div>
         </div>
         <div className="flex flex-col gap-3">
           <h2 className="text-2xl font-bold overflow-clip max-h-16">{title}</h2>
