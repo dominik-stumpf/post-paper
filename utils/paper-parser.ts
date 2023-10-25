44eef2e338d386bd07323f12e688bd82710d9dbd
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-25 17:15:51 +0200

    removed unused code

--- utils/paper-parser.ts
+++ utils/paper-parser.ts
@@ -17,48 +17,25 @@ export class PaperParser {
   constructor(private readonly paper: string) {}
 
   parseCard() {
-    // const processor = unified().use(remarkParse);
-    // const mdastTree = processor.parse(this.paper);
-
-    const mdastTree = fromMarkdown(this.paper);
-
-    // const heading = mdastTree.children[0];
-
-    // if (heading?.type === 'heading' && heading.depth === 1) {
-    //   title = heading.children
-    //     .flatMap((context) => {
-    //       if (Object.hasOwn(context, 'value')) {
-    //         return context.value;
-    //       }
-    //       // if (context.type === 'text') {
-    //       //   return context.value;
-    //       // } else {
-    //       //   // console.log('context', context);
-    //       // }
-    //       return '';
-    //     })
-    //     .join('');
-    // }
-
-    // Markdown({ children: this.paper });
+    const mdastTree = fromMarkdown(this.paper.slice(0, 256));
     let title = '';
     let content = '';
     let isIteratingHeader = false;
-    let iterationCount = 0;
 
     function transform(
       ...[node, index, parent]: Parameters<Visitor>
     ): ReturnType<Visitor> {
-      iterationCount += 1;
-      console.log('visiting', node, iterationCount);
-      if (iterationCount > 16) {
-        return EXIT;
-      }
       const isRootChild = parent?.type === 'root';
 
-      if (isRootChild && index === 0 && node.type === 'heading') {
+      if (
+        isRootChild &&
+        index === 0 &&
+        node.type === 'heading' &&
+        node.depth === 1
+      ) {
         isIteratingHeader = true;
       }
+
       if (
         isIteratingHeader &&
         isRootChild &&
@@ -69,61 +46,19 @@ export class PaperParser {
       }
 
       if (isIteratingHeader) {
-        // console.log('iterating header', node);
         if ('value' in node) {
           title += node.value;
         }
       } else if ('value' in node) {
-        // console.log(node);
-        content += node.value.replaceAll('\n', ' ');
+        content += node.value;
       }
     }
 
     visit(mdastTree, transform);
 
+    title = title.replaceAll('\n', ' ');
+    content = content.replaceAll('\n', ' ');
+
     return { title: title, content: content };
   }
-
-  truncateRecursively(
-    element: simplifiedTextNode[] | undefined,
-    truncateCharCount: number,
-  ) {
-    if (truncateCharCount <= 0 || element === undefined) return;
-
-    let tracker = truncateCharCount;
-
-    function truncate(elem: simplifiedTextNode[]) {
-      for (let i = elem.length - 1; i >= 0; i -= 1) {
-        if (tracker === 0) {
-          return;
-        }
-
-        const { text, children } = elem[i];
-
-        if (children) {
-          truncate(children);
-          if (children[0] === undefined) {
-            delete elem[i];
-          }
-        }
-
-        if (text) {
-          if (tracker >= text.length) {
-            tracker -= text.length;
-            delete elem[i];
-          } else {
-            elem[i].text = text.slice(0, Math.abs(tracker - text.length));
-            tracker = 0;
-          }
-        }
-      }
-    }
-
-    truncate(element);
-  }
-}
-
-interface simplifiedTextNode {
-  text?: string;
-  children?: simplifiedTextNode[];
 }
