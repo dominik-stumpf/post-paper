8f7f69076f5e68206d31e4e179a4a74ed0240cd6
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-31 00:56:17 +0100

    removed pwa

--- next.config.js
+++ next.config.js
@@ -15,11 +15,13 @@ const nextConfig = {
   },
 }
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === 'development',
-});
+module.exports = nextConfig;
 
-module.exports = withPWA(
-  nextConfig
-);
+// const withPWA = require("@ducanh2912/next-pwa").default({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === 'development',
+// });
+
+// module.exports = withPWA(
+//   nextConfig
+// );
