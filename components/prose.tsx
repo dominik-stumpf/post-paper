58e42ca9e006a8c2f4bc19b704d85352ccab6cc8
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-30 20:13:09 +0100

    removed unused file

--- components/prose.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-
-export const proseClassName =
-  'max-w-prose prose lg:prose-lg w-full prose-fuchsia prose-invert';
-
-export function Prose({ children }: { children: ReactNode }) {
-  return <article className={proseClassName}>{children}</article>;
-}
