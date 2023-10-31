c3b1e591b5a0894689fe1500b9d9d85fa6303d72
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-31 11:00:34 +0100

    updated next config

--- next.config.js
+++ next.config.js
@@ -15,13 +15,11 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig;
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+});
 
-// const withPWA = require("@ducanh2912/next-pwa").default({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === 'development',
-// });
-
-// module.exports = withPWA(
-//   nextConfig
-// );
+module.exports = withPWA(
+  nextConfig
+);
