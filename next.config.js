f331c1a845064797f46e4f6de703a66a0c97e923
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-31 12:48:14 +0100

    removed comments

--- next.config.js
+++ next.config.js
@@ -1,31 +1,3 @@
-// const pwa = require('@ducanh2912/next-pwa').default;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ['api.dicebear.com'],
-//   },
-//   experimental: {
-//     serverActions: true,
-//   },
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.md$/,
-//       use: 'raw-loader',
-//     });
-//     return config;
-//   },
-// }
-
-// const withPWA = pwa({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === 'development',
-// });
-
-// module.exports = withPWA(
-//   nextConfig
-// );
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
