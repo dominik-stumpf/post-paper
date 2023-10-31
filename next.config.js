4b00ba786c76d13e161bdc55a10810cdcb18e556
Dominik Stumpf <s.dominik.work@gmail.com>
2023-10-31 11:28:05 +0100

    downgraded pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,44 @@
-import pwa from '@ducanh2912/next-pwa'
+// const pwa = require('@ducanh2912/next-pwa').default;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['api.dicebear.com'],
+//   },
+//   experimental: {
+//     serverActions: true,
+//   },
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /\.md$/,
+//       use: 'raw-loader',
+//     });
+//     return config;
+//   },
+// }
+
+// const withPWA = pwa({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === 'development',
+// });
+
+// module.exports = withPWA(
+//   nextConfig
+// );
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['api.dicebear.com'],
   },
   experimental: {
     serverActions: true,
   },
-  webpack: (config) => {
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.externals.push('sharp')
+    }
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
@@ -17,7 +47,7 @@ const nextConfig = {
   },
 }
 
-const withPWA = pwa({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === 'development',
 });
