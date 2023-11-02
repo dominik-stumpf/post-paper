bb65e24f58061e875fe28ae707821fd2174b4478
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-02 20:11:30 +0100

    responsive navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,20 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { PageRoot } from './page-root';
 import { BrandLogo } from './brand-logo';
+import Edit3 from '@geist-ui/icons/edit3';
+import { Avatar } from './avatar';
+import { ReactNode } from 'react';
+import Menu from '@geist-ui/icons/menu';
 
 export const dynamic = 'force-dynamic';
+
+function NavbarActions({ children }: { children: ReactNode }) {
+  return <div className="md:flex gap-8 items-center hidden">{children}</div>;
+}
+
+function MenuActions({ children }: { children: ReactNode }) {
+  return <div className="md:hidden">{children}</div>;
+}
 
 export async function Navbar() {
   const {
@@ -15,28 +27,39 @@ export async function Navbar() {
   }).auth.getSession();
 
   return (
-    <nav className="isolate flex justify-center w-full h-header sticky top-0 bg-black/25 backdrop-blur-lg backdrop-saturate-150 shadow-[inset_rgba(255_255_255_/_0.15)_0_-1px] z-40">
+    <header className="isolate flex justify-center w-full h-header sticky top-0 bg-black/25 backdrop-blur-lg backdrop-saturate-150 shadow-[inset_rgba(255_255_255_/_0.15)_0_-1px] z-40">
       <PageRoot onlyHorizontalBoundary>
-        <div className="flex items-center justify-between w-full h-full p-3 text-sm text-foreground">
+        <div className="flex items-center justify-between w-full h-full py-3 text-base">
           <BrandLogo />
-          {session?.user ? (
-            <>
-              <div className="flex items-center gap-4">
-                Hey, {session.user.user_metadata.name}!
-                <Link href="/write">write</Link>
-                <LogoutButton />
-              </div>
-            </>
-          ) : (
-            <Link
-              href="/login"
-              className="flex px-3 py-2 ml-auto no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
-            >
-              Login
-            </Link>
-          )}
+          <NavbarActions>
+            {session?.user ? (
+              <>
+                <Link href="/write" className="flex gap-2">
+                  <Edit3 />
+                  Write
+                </Link>
+                <Avatar
+                  imageSrc={session.user.user_metadata.avatar_url}
+                  size="base"
+                />
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className="flex px-3 py-2 ml-auto no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
+                >
+                  Login
+                </Link>
+                <Link href="/signup">Signup</Link>
+              </>
+            )}
+          </NavbarActions>
+          <MenuActions>
+            <Menu className="pointer" />
+          </MenuActions>
         </div>
       </PageRoot>
-    </nav>
+    </header>
   );
 }
