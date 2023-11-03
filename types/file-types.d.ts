770933f046e371bbafbea1bf5cad8d380cdf5ed0
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-03 12:24:29 +0100

    type declaration for rsvg

--- types/file-types.d.ts
+++ types/file-types.d.ts
@@ -1,1 +1,8 @@
 declare module '*.md';
+declare module '*.svg' {
+  import React from 'react';
+
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+  export default content;
+}
