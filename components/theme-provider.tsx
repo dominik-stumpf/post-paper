c9ab13c945e7a59266769051248ed14924676207
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-04 08:38:12 +0100

    theme change

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,7 +6,7 @@ import type { ThemeProviderProps } from 'next-themes/dist/types';
 export function ThemeProvider({ children }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute="class"
+      attribute="data-theme"
       defaultTheme="system"
       enableSystem
       disableTransitionOnChange
