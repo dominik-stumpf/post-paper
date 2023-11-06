e8d16a849350acd32b46f067fc02eafc13363518
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-06 12:29:08 +0100

    navbar responsive

--- components/navbar/user-actions.tsx
+++ components/navbar/user-actions.tsx
@@ -10,10 +10,11 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { LogOut, PenSquare, User } from 'lucide-react';
+import { LogOut, PenSquare, User, Menu } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Session } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
+import { ReactNode } from 'react';
 
 export function UserActions({ session }: { session: Session | null }) {
   const router = useRouter();
@@ -27,62 +28,78 @@ export function UserActions({ session }: { session: Session | null }) {
   if (session?.user) {
     return (
       <>
-        <Button asChild variant={'default'} size="sm">
-          <Link href="/write" className="flex gap-2">
-            <PenSquare className="w-4 h-4" />
-            Write
-          </Link>
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant={'ghost'} size="icon" className="rounded-full">
-              <Avatar>
-                <AvatarImage
-                  src={session.user.user_metadata.avatar_url}
-                  alt="User profile picture"
-                />
-                <AvatarFallback />
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>
-              <div>{session.user.user_metadata.name}</div>
-              <div className="font-normal text-muted-foreground">
-                {session.user.email}
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="flex justify-between" disabled>
-              Profile
-              <User className="w-4 h-4" />
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              className="flex justify-between"
-              onClick={signOutUser}
-            >
-              Log out
-              <LogOut className="w-4 h-4" />
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="hidden md:block">
+          <Button asChild variant={'default'} size="sm">
+            <Link href="/write" className="flex gap-2">
+              <PenSquare className="w-4 h-4" />
+              Write
+            </Link>
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant={'ghost'} size="icon" className="rounded-full">
+                <Avatar>
+                  <AvatarImage
+                    src={session.user.user_metadata.avatar_url}
+                    alt="User profile picture"
+                  />
+                  <AvatarFallback />
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                <div>{session.user.user_metadata.name}</div>
+                <div className="font-normal text-muted-foreground">
+                  {session.user.email}
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="flex justify-between" disabled>
+                Profile
+                <User className="w-4 h-4" />
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex justify-between"
+                onClick={signOutUser}
+              >
+                Log out
+                <LogOut className="w-4 h-4" />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <ActionMenu>a</ActionMenu>
       </>
     );
   }
 
   return (
-    <nav className="space-x-3">
-      <Button variant={'ghost'} asChild size="sm">
-        <Link
-          href="/login"
-          className="flex px-3 py-2 ml-auto no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
-        >
-          Log in
-        </Link>
+    <>
+      <nav className="hidden space-x-3 md:block">
+        <Button variant={'ghost'} asChild size="sm">
+          <Link
+            href="/login"
+            className="flex px-3 py-2 ml-auto no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
+          >
+            Log in
+          </Link>
+        </Button>
+        <Button variant={'outline'} size="sm" asChild>
+          <Link href="/signup">Sign up</Link>
+        </Button>
+      </nav>
+      <ActionMenu>a</ActionMenu>
+    </>
+  );
+}
+
+function ActionMenu({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Button className="md:hidden" variant={'ghost'} size={'icon'}>
+        <Menu />
       </Button>
-      <Button variant={'outline'} size="sm" asChild>
-        <Link href="/signup">Sign up</Link>
-      </Button>
-    </nav>
+    </div>
   );
 }
