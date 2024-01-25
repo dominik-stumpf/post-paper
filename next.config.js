b541ce1567af8cce75329247e6cc1c9d7d67635e
Dominik Stumpf <s.dominik.work@gmail.com>
2024-01-25 16:22:20 +0100

    build: remove service workers

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   images: {
     domains: ['api.dicebear.com'],
   },
-  webpack: (config, {isServer}) => {
+  webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.externals.push('sharp')
+      config.externals.push('sharp');
     }
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ['@svgr/webpack'],
     });
     config.module.rules.push({
       test: /\.md$/,
@@ -18,13 +18,11 @@ const nextConfig = {
     });
     return config;
   },
-}
+};
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === 'development',
-});
+// const withPWA = require("@ducanh2912/next-pwa").default({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === 'development',
+// });
 
-module.exports = withPWA(
-  nextConfig
-);
+module.exports = nextConfig;
