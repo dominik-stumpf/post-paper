61832eca91e50d30162d9fd99d8a8960f6cd8d70
Dominik Stumpf <s.dominik.work@gmail.com>
2024-01-26 14:59:08 +0100

    chore: remove unused config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['api.dicebear.com'],
   },
@@ -19,10 +18,5 @@ const nextConfig = {
     return config;
   },
 };
-
-// const withPWA = require("@ducanh2912/next-pwa").default({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === 'development',
-// });
 
 module.exports = nextConfig;
