6e598316c8b15a427e038c3d62834adbd5466397
Dominik Stumpf <s.dominik.work@gmail.com>
2024-01-26 16:53:45 +0100

    chore: update css vars, remove unused deps

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const nextConfig = {
       config.externals.push('sharp');
     }
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
     });
--- types/file-types.d.ts
+++ types/file-types.d.ts
@@ -1,8 +1,1 @@
 declare module '*.md';
-declare module '*.svg' {
-  import type React from 'react';
-
-  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-
-  export default content;
-}
