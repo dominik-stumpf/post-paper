d9b044a58ef5c9bf8ce6e4c0551df447cebe1b1d
Dominik Stumpf <s.dominik.work@gmail.com>
2024-02-01 16:21:42 +0100

    feat: create editor view shift

--- app/(editor)/write/page.tsx
+++ app/(editor)/write/page.tsx
@@ -3,10 +3,17 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { type FormEvent, useState } from 'react';
 import { EditorMemo } from './editor';
-import reactHooksPost from '@/public/markdown/react-hooks-post-validate.md';
+import reactHooksPost from '@/public/markdown/react-hooks-post.md';
 import { useToast } from '@/components/ui/use-toast';
 import { Preview } from './preview';
 import { PageMargin } from '@/components/page-margin';
+import {
+  Forward,
+  HelpCircle,
+  Settings,
+  SplitSquareVertical,
+} from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const initialMarkdown = reactHooksPost;
 
@@ -52,15 +59,31 @@ export default function Page() {
   }
 
   return (
-    <div className="relative h-remaining overflow-hidden">
+    <div className="relative w-full">
       <output className="pointer-events-none flex select-none flex-col items-center">
-        <PageMargin verticalMargin>
-          <Preview markdown={initialMarkdown} positionOffset={0} />
+        <PageMargin verticalMargin className="mb-[30dvh]">
+          <Preview markdown={initialMarkdown} positionOffset={1024} />
         </PageMargin>
       </output>
-      <main className="absolute bottom-8 left-1/2 h-3/4 w-full max-w-screen-2xl -translate-x-1/2 overflow-hidden rounded border bg-background/60 px-8 py-4 ring-ring ring-offset-2 ring-offset-background backdrop-blur transition-colors focus-within:ring-2 hover:border-muted-foreground focus-visible:outline-none">
-        <EditorMemo />
-      </main>
+      <div className="fixed inset-x-0 bottom-0 m-4 h-3/4 select-none">
+        <main className="relative bottom-0 left-1/2 h-full w-full max-w-screen-2xl -translate-x-1/2 translate-y-3/4 overflow-hidden rounded border bg-background/60 px-12 py-4 ring-ring ring-offset-2 ring-offset-background backdrop-blur transition-transform ease-in-out focus-within:translate-y-0 focus-within:ring-2 hover:border-muted-foreground focus-visible:outline-none">
+          <EditorMemo />
+          <aside className="absolute bottom-4 right-4 flex flex-col items-end gap-4">
+            <Button size="icon" variant="outline">
+              <HelpCircle />
+            </Button>
+            <Button size="icon" variant="outline">
+              <Settings />
+            </Button>
+            <Button size="icon" variant="outline">
+              <SplitSquareVertical />
+            </Button>
+            <Button size="icon">
+              <Forward />
+            </Button>
+          </aside>
+        </main>
+      </div>
     </div>
   );
   // return (
